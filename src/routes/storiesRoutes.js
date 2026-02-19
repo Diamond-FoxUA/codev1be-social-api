2414f68c652ffb7fe790781d90f6da7a0b022e88
Yevhiniia Ripa <evgeniarepa@gmail.com>
2026-02-19 22:57:40 +0200

    feat: add authentication and validation to favorite stories endpoints

--- src/routes/storiesRoutes.js
+++ src/routes/storiesRoutes.js
@@ -1,14 +1,28 @@
 import { Router } from 'express';
+import { celebrate } from 'celebrate';
 import {
   getAllStories,
   addToFavorites,
   removeFromFavorites,
 } from '../controllers/storiesController.js';
+import { storyIdSchema } from '../validations/storiesValidation.js';
+import { authenticate } from '../middleware/authenticate.js';
 
 const router = Router();
 
+router.use('/favorite', authenticate);
+
 router.get('/api/stories', getAllStories);
-router.post('/api/stories/favorite/:storyId', addToFavorites);
-router.delete('/api/stories/favorite/:storyId', removeFromFavorites);
+
+router.post(
+  '/api/stories/favorite/:storyId',
+  celebrate(storyIdSchema),
+  addToFavorites,
+);
+router.delete(
+  '/favorite/:storyId',
+  celebrate(storyIdSchema),
+  removeFromFavorites,
+);
 
 export default router;
