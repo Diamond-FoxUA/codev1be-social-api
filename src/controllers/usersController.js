739d4ef7bf98fd45ffdec57c7591eb06347322a0
Dmytro Farbun <dimofarb@gmail.com>
2026-02-19 23:19:17 +0200

    chore: Remove TODO comments in usersController
    Removed TODO comments for user story functions.

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -3,10 +3,6 @@ export const getAllUsers = async (req, res) => {
   res.status(200).json({ message: 'All users received' });
 };
 
-// TODO: getCurrentUserStories (need to shorten the const name) – this should give us all stories that current user has
-// TODO: getSavedStories – should show stories user have saved
-
-// so getCurrentUser gets us info about user 
 export const getCurrentUser = async (req, res) => {
   const { _id, name, avatarUrl, description } = req.user;
 
