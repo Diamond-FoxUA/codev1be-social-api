2c7f5faafc1fc713e2be1424e9574539eea5f9fe
Dmytro Farbun <dimofarb@gmail.com>
2026-02-21 00:28:41 +0200

    feat(userRoutes): add userIdSchema to getUserById endpoint

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { celebrate } from "celebrate";
 import { getUsers, getUserById } from "../controllers/usersController.js";
-import { getUsersSchema } from "../validations/usersValidation.js";
+import { getUsersSchema, userIdSchema } from "../validations/usersValidation.js";
 
 const router = Router();
 
 router.get("/", celebrate(getUsersSchema), getUsers);
-router.get("/:userId", celebrate(), getUserById);
+router.get("/:userId", celebrate(userIdSchema), getUserById);
 
 export default router;
