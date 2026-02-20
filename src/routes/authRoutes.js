e92fdf8cd9021ec959f7d94f4df3e28e89de69c7
Dmytro Farbun <dimofarb@gmail.com>
2026-02-20 20:57:00 +0200

    refactor: delete unknown and duplicate files

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,15 +1,12 @@
 import { Router } from 'express';
-import { loginUser, registerUser, logoutUser } from '../controllers/authController.js';
 import { celebrate } from 'celebrate';
-import {
-  loginUserSchema,
-  registerUserSchema,
-} from '../validations/authValidation.js';
-import { authMiddleware } from '../middleware/authMiddleware.js';
+import { loginUser, registerUser, logoutUser } from '../controllers/authController.js';
+import { loginUserSchema, registerUserSchema } from '../validations/authValidation.js';
 
 const router = Router();
 
 router.post('/register', celebrate(registerUserSchema), registerUser);
 router.post('/login', celebrate(loginUserSchema), loginUser);
 router.post('/logout', logoutUser);
+
 export default router;
