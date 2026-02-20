8402df71cd9a97882f0188fc425e8d4bb3f0bd3f
Eugene O. (id753) <mimywhy@gmail.com>
2026-02-20 18:16:56 +0200

    Реализовано  функцию сброса пароля и шаблоны электронных писем.

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,8 +1,29 @@
-import { Router } from "express";
-import { registerUser } from '../controllers/authController.js';
+import { Router } from 'express';
+import {
+  registerUser,
+  requestResetEmail,
+  resetPassword,
+} from '../controllers/authController.js';
+import { celebrate } from 'celebrate';
+import {
+  requestResetEmailSchema,
+  resetPasswordSchema,
+} from '../validations/authValidation.js';
 
 const router = Router();
 
 router.post('/api/register', registerUser);
+
+router.post(
+  '/auth/request-reset-email',
+  celebrate(requestResetEmailSchema),
+  requestResetEmail,
+);
+
+router.post(
+  '/auth/reset-password',
+  celebrate(resetPasswordSchema),
+  resetPassword,
+);
 
 export default router;
