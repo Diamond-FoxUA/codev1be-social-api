8fb40b59d68044fc788d92de945bef941d2ce280
Dmytro Farbun <dimofarb@gmail.com>
2026-02-21 18:50:37 +0200

    Limit description length to 150 characters

--- src/validations/userValidation.js
+++ src/validations/userValidation.js
@@ -3,6 +3,6 @@ import { Joi, Segments } from 'celebrate';
 export const updateUserSchema = {
   [Segments.BODY]: Joi.object({
     name: Joi.string().min(3).max(32),
-    description: Joi.string().max(200).allow(''),
+    description: Joi.string().max(150).allow(''),
   }),
 };
