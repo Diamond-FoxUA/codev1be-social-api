04b25b6d00ff948c2b788187bd803f4e2ceab540
Dmytro Farbun <dimofarb@gmail.com>
2026-02-21 19:09:14 +0200

    fix(userValidation): moved updateUserSchema to userValidation.js and delete duplicate of that file

--- src/validations/userValidation.js
+++
@@ -1,8 +0,0 @@
-import { Joi, Segments } from 'celebrate';
-
-export const updateUserSchema = {
-  [Segments.BODY]: Joi.object({
-    name: Joi.string().min(3).max(32),
-    description: Joi.string().max(150).allow(''),
-  }),
-};
