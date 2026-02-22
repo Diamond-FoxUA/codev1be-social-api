1c22abdc7c37f075745e22c0012b0605a2be5cd7
Dmytro Farbun <dimofarb@gmail.com>
2026-02-22 21:28:28 +0200

    Rename getSavedStories to getFavouriteStories

--- src/controllers/storiesController.js
+++ src/controllers/storiesController.js
@@ -130,7 +130,7 @@ export const removeFromFavorites = async (req, res) => {
   res.status(200).json(updatedUser.savedArticles);
 };
 
-export const getSavedStories = async (req, res) => {
+export const getFavouriteStories = async (req, res) => {
   const page = Number(req.query.page) || 1;
   const perPage = Number(req.query.perPage) || 4;
   const skip = (page - 1) * perPage;
