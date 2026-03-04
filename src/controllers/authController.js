dbce7741f969669b5dc0b28940ca2d145cb22e90
Dmytro Farbun <dimofarb@gmail.com>
2026-03-04 18:38:37 +0200

    Update authController.js

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -60,38 +60,27 @@ export const logoutUser = async (req, res) => {
   res.status(204).send();
 };
 
-import createHttpError from "http-errors";
-import { Session } from "../models/session.js";
-import { createSession, setSessionCookies } from "../services/session.js";
-
 export const refreshUserSession = async (req, res) => {
-  const { refreshToken } = req.cookies;
+  const { sessionId, refreshToken } = req.cookies;
 
-  if (!refreshToken) {
-    throw createHttpError(401, "No refresh token");
-  }
-
-  const session = await Session.findOne({ refreshToken });
-
+  const session = await Session.findOne({
+    _id: sessionId,
+    refreshToken: refreshToken
+  });
   if (!session) {
     throw createHttpError(401, "Session not found");
   }
 
-  const isTokenExpired =
-    new Date() > new Date(session.refreshTokenValidUntil);
-
+  const isTokenExpired = new Date() > new Date(session.refreshTokenValidUntil);
   if (isTokenExpired) {
-    await Session.deleteOne({ _id: session._id });
-    throw createHttpError(401, "Refresh token expired");
+    throw createHttpError(401, "Session token expired");
   }
 
-  await Session.deleteOne({ _id: session._id });
+  await session.deleteOne();
 
   const newSession = await createSession(session.userId);
-
   setSessionCookies(res, newSession);
 
-  res.status(200).json({
-    message: "Session refreshed",
-  });
+  res.status(200).json({ message: "Session refreshed" });
 };
+
