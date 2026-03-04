c718cf3d49177557b7ebd7257116098cfc154481
Dmytro Farbun <dimofarb@gmail.com>
2026-03-04 19:10:07 +0200

    Update authenticate.js

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -1,32 +1,55 @@
 import createHttpError from 'http-errors';
 import { Session } from '../models/session.js';
 import { User } from '../models/user.js';
+import { createSession, setSessionCookies } from '../utils/session.js';
 
 export const authenticate = async (req, res, next) => {
-  if (!req.cookies.accessToken) {
-    throw createHttpError(401, 'Missing access token');
+  const { accessToken, refreshToken, sessionId } = req.cookies;
+
+  let session = null;
+
+  if (accessToken) {
+    session = await Session.findOne({ accessToken });
+
+    if (session) {
+      const isAccessExpired =
+        new Date() > new Date(session.accessTokenValidUntil);
+
+      if (!isAccessExpired) {
+        const user = await User.findById(session.userId);
+        req.user = user;
+        return next();
+      }
+    }
   }
 
-  const session = await Session.findOne({
-    accessToken: req.cookies.accessToken,
+  if (!refreshToken || !sessionId) {
+    throw createHttpError(401, 'Missing refresh token');
+  }
+
+  session = await Session.findOne({
+    _id: sessionId,
+    refreshToken,
   });
 
   if (!session) {
     throw createHttpError(401, 'Session not found');
   }
 
-  const isAccessTokenExpired =
-    new Date() > new Date(session.accessTokenValidUntil);
+  const isRefreshExpired =
+    new Date() > new Date(session.refreshTokenValidUntil);
 
-  if (isAccessTokenExpired) {
-    throw createHttpError(401, 'Access token expired');
+  if (isRefreshExpired) {
+    throw createHttpError(401, 'Refresh token expired');
   }
+
+  await session.deleteOne();
+
+  const newSession = await createSession(session.userId);
+
+  setSessionCookies(res, newSession);
 
   const user = await User.findById(session.userId);
-
-  if (!user) {
-    throw createHttpError(401);
-  }
 
   req.user = user;
 
